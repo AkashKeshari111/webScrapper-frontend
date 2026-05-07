5c7cfd951a4e763dde9b512bd61b7153408dd138
AkashKeshari111 <TechTeam-studentdiwan>
2026-05-07 17:25:48 +0530

    setup frontend app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Bookmarks from "./pages/Bookmarks";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/bookmarks" element={<Bookmarks />} />
+    </Routes>
+  );
+}
+
+export default App;
